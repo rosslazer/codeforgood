1fee0b4f8109d19f63625ddc36fb3973938b9f82
rosslazer <Sputnik@sputnik.local>
2013-02-23 13:15:42 -0500

    Fixing model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,12 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, 
-         :first_name, :last_name, :address, :branch, :role, 
-         :bio, :request_id
-
+         :recoverable, :rememberable, :trackable, :validatable
+         
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+  :first_name, :last_name, :address, :branch, :role, :bio, :request_id
   # attr_accessible :title, :body
 
 
