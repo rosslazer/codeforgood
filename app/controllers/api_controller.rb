9ea1823e43a9d2118b681c29c16097479f44961b
rosslazer <Sputnik@sputnik.local>
2013-02-23 21:46:42 -0500

    Got a post model working?

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -13,5 +13,24 @@ class ApiController < ApplicationController
 	end
 
 
-	
+	def new_item
+
+		@tool = Tool.new
+		@tool = Tool.new(params[:params])
+			respond_to do |format|
+		      if @location.save
+		        format.html { redirect_to @tool, :notice => 'Location was successfully created.' }
+		        format.json { render :json => @tool, :status => :created, :location => @location }
+		      else
+		        format.html { render :action => "new" }
+		        format.json { render :json => @tool.errors, :status => :unprocessable_entity }
+		      end
+		    end
+
+
+	end
+
+
+\
+
 end
