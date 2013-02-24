5e365d8f92c23e4bd0f30f0fd487c25eaef18777
rosslazer <Sputnik@sputnik.local>
2013-02-24 09:29:06 -0500

    Model stuff

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
 	has_many :tools
-   attr_accessible :name, :location, :active, :description
+   attr_accessible :name, :location, :active, :description, :id
     
 end
